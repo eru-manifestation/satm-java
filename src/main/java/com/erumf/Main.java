4da3b15d5844f8b12591c0935725b82113354716
eru-manifestation <129109001+eru-manifestation@users.noreply.github.com>
2025-02-20 19:07:28 +0100

    Implement logging functionality and initialize game paths in Main class

--- src/main/java/com/erumf/Main.java
+++ src/main/java/com/erumf/Main.java
@@ -2,35 +2,33 @@ package com.erumf;
 
 import org.jgrapht.Graph;
 
+import com.erumf.cards.locations.Locations2;
 import com.erumf.elements.Location;
-import com.erumf.events.GameListener;
-import com.erumf.events.GameListener.EventType;
-import com.erumf.utils.GameProperty;
+import com.erumf.log.Logger;
 import com.erumf.utils.graph.DefaultPathEdge;
 
 public class Main {
-    static Graph<Location, DefaultPathEdge> pathsGraph;
+    static Graph<Location, DefaultPathEdge> pathsGraph = Locations2.generateDefaultLocations();
+    public static Player player1 = new Player();
+    public static Player player2 = new Player();
 
     public static void main(String[] args) {
-        // pathsGraph = Locations2.generateDefaultLocations();
 
-        // System.out.println("Graph of locations: " + pathsGraph.edgeSet().size() + " edges");
+        Logger.info("Starting game...");
 
-        // System.out.println("Hello world!");
+        // GameProperty<String> property = new GameProperty<>("hello", "world", null);
+        // GameProperty<String> property2 = new GameProperty<>("goodbye", "world", null);
+        // GameListener.create(EventType.IN,
+        //     (p, v) -> p.getName().equals("hello"), 
+        //     (p, v) -> {
+        //         System.out.println("Hello " + v + "!");
+        //         return true;
+        //     }
+        // );
+        // System.out.println(property.getValue());
 
-        GameProperty<String> property = new GameProperty<>("hello", "world", null);
-        GameProperty<String> property2 = new GameProperty<>("goodbye", "world", null);
-        GameListener.create(EventType.IN,
-            (p, v) -> p.getName().equals("hello"), 
-            (p, v) -> {
-                System.out.println("Hello " + v + "!");
-                return true;
-            }
-        );
-        System.out.println(property.getValue());
-
-        property.setValue("WORLD");
-        System.out.println(property.getValue());
+        // property.setValue("WORLD");
+        // System.out.println(property.getValue());
         
     }
 
---
+++ src/main/java/com/erumf/log/Logger.java
@@ -0,0 +1,26 @@
+package com.erumf.log;
+
+public class Logger {
+
+    public static void info(String message) {
+        System.out.println();
+        printWithTag("[INFO]", message);
+    }
+
+    public static void warning(String message) {
+        System.out.println();
+        printWithTag("[WARNING]", message);
+    }
+
+    public static void error(String message) {
+        System.out.println();
+        printWithTag("[ERROR]", message);
+    }
+
+    private static void printWithTag(String tag, String message) {
+        String[] lines = message.split("\n");
+        for (String line : lines) {
+            System.out.println(tag + " " + line);
+        }
+    }
+}
