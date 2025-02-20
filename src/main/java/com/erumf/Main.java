7628cd864536d76a118e178273d3e07996ce661e
eru-manifestation <129109001+eru-manifestation@users.noreply.github.com>
2025-02-20 20:41:01 +0100

    Fixed, cards left added

--- src/main/java/com/erumf/Main.java
+++ src/main/java/com/erumf/Main.java
@@ -1,8 +1,11 @@
 package com.erumf;
 
+import java.util.List;
+
 import org.jgrapht.Graph;
 
 import com.erumf.cards.locations.Locations2;
+import com.erumf.cards.starter.deck.StarterDeck;
 import com.erumf.elements.Location;
 import com.erumf.log.Logger;
 import com.erumf.utils.graph.DefaultPathEdge;
@@ -15,6 +18,11 @@ public class Main {
     public static void main(String[] args) {
 
         Logger.info("Starting game...");
+        List<Basic> deck1 = StarterDeck.defaultDeck1(player1);
+        List<Basic> deck2 = StarterDeck.defaultDeck2(player2);
+
+        Logger.info("Deck 1: " + deck1.size());
+        Logger.info("Deck 2: " + deck2.size());
 
         // GameProperty<String> property = new GameProperty<>("hello", "world", null);
         // GameProperty<String> property2 = new GameProperty<>("goodbye", "world", null);
--- src/main/java/com/erumf/cards/starter/deck/StarterDeck.java
+++ src/main/java/com/erumf/cards/starter/deck/StarterDeck.java
@@ -166,7 +166,7 @@ public class StarterDeck {
         deck.add(new DarkQuarrels(player2)); // S18
         deck.add(new Saruman(player2)); // S19
         deck.add(new LesserSpiders(player2)); // S20
-        deck.add(new Orcrist(player2)); // S20
+        deck.add(new Orcrist(player2)); // S20 TODO: THE WEB HAS A MISTAKE, ONE OF THIS IS NOT CORRECT
         deck.add(new Ghouls(player2)); // S21
         deck.add(new HalflingStealth(player2)); // S22
         deck.add(new Annalena(player2)); // S23
@@ -187,12 +187,14 @@ public class StarterDeck {
         deck.add(new Huorn(player2)); // S38
         deck.add(new RiskyBlow(player2)); // S39
         deck.add(new Wolves(player2)); // S40 TODO: This is NOT the real S40 card, no information on the web
+        deck.add(new River(player2)); // S41
         deck.add(new Celeborn(player2)); // S42
         deck.add(new Block(player2)); // S43
         deck.add(new SwordOfGondolin(player2)); // S44
         deck.add(new TemperingFriendship(player2)); // S45
         deck.add(new LesserSpiders(player2)); // S46
         deck.add(new HalflingStrength(player2)); // S47
+        deck.add(new River(player2)); // S48
         deck.add(new Concealment(player2)); // S49
         deck.add(new Ghosts(player2)); // S50
         deck.add(new Elrohir(player2)); // S51
