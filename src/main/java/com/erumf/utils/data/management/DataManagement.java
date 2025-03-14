674af67d96598d2dec335aa08e63671bd5e892cb
eru-manifestation <129109001+eru-manifestation@users.noreply.github.com>
2025-03-14 21:11:06 +0100

    feat: Update subObjects method in DataManagement to filter cards by type and subtype; adjust file output path for improved resource management

--- src/main/java/com/erumf/utils/data/management/DataManagement.java
+++ src/main/java/com/erumf/utils/data/management/DataManagement.java
@@ -12,28 +12,34 @@ import org.json.JSONObject;
 import org.json.JSONTokener;
 
 public class DataManagement {
-
     
-    private static void subObjects(){
-        try (InputStream is = new FileInputStream("C:\\Users\\Pablo\\Desktop\\VSCode Workspace\\erus-will\\src/main/resources/cards.json")) {
+    public static void subObjects(String type, String subtype){
+        try (InputStream is = new FileInputStream("src/main/resources/dev/cards.json")) {
             JSONObject jsonObject = new JSONObject(new JSONTokener(is));
             JSONObject twProperty = jsonObject.getJSONObject("TW").getJSONObject("cards");
+            
             
             JSONArray filtered = new JSONArray();
             Iterator<String> keys = twProperty.keys();
             while (keys.hasNext()) {
                 String key = keys.next();
                 JSONObject card = twProperty.getJSONObject(key);
-                if ("Site".equals(card.getString("type"))) {
-                    filtered.put(card);
+                if (type.equals(card.getString("type"))) {
+                    if (subtype!=null) {
+                        String cardSubtype = card.getJSONObject("attributes").getString("subtype");
+                        if (cardSubtype.equals(subtype)) {
+                            filtered.put(card);
+                        }
+                    }else filtered.put(card);
                 }
             }
-
-            try (OutputStream os = new FileOutputStream("C:\\Users\\Pablo\\Desktop\\VSCode Workspace\\erus-will\\src/main/resources/TWSites.json")) {
+            String fileName = type+ (subtype==null? "":"-"+subtype) +".json";
+            try (OutputStream os = new FileOutputStream("src/main/resources/dev/"+fileName)) {
                 os.write(filtered.toString(4).getBytes());
-                System.out.println("TW Property written to resources/TWSites.json: "+filtered.length()+" objects");
+                System.out.println("TW Property written to src/main/resources/dev/"+fileName+" "+filtered.length()+" objects");
             }
         } catch (IOException e) {
+            System.out.println("Unable to filter cards");
             e.printStackTrace();
         }
     }
