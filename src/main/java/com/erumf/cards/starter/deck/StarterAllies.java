903860192bfb65a7fd58fe10e98fd2cb3d463404
eru-manifestation <129109001+eru-manifestation@users.noreply.github.com>
2025-03-15 18:34:31 +0100

    feat: simplify Ally constructor by removing corruption and influence parameters; update StarterAllies to reflect changes

--- src/main/java/com/erumf/cards/starter/deck/StarterAllies.java
+++ src/main/java/com/erumf/cards/starter/deck/StarterAllies.java
@@ -4,14 +4,13 @@ import com.erumf.Player;
 import com.erumf.elements.Ally;
 
 public class StarterAllies {
-    //TODO: Implementar todos los atributos de los aliados
     // ALIADOS DE GANDALF
     /**
      * G31 TW077 Aliado Ramaviva
      */
     public static class Quickbeam extends Ally {
         public Quickbeam(Player player) {
-            super(player, 2, 0, 0, 0, 0, 0);
+            super(player, 2, 3, 9, 6);
         }
     }
 
@@ -21,7 +20,7 @@ public class StarterAllies {
      */
     public static class Gollum extends Ally {
         public Gollum(Player player) {
-            super(player, 2, 0, 0, 0, 0, 0);
+            super(player, 2, 4, 9, 2);
         }
     }
 }
--- src/main/java/com/erumf/elements/Ally.java
+++ src/main/java/com/erumf/elements/Ally.java
@@ -3,8 +3,8 @@ package com.erumf.elements;
 import com.erumf.Player;
 
 public class Ally extends Resource {
-    public Ally(Player player, int mp, int corruption, int influence, int mind, int body, int prowess) {
-        super(player, mp, corruption, influence, mind, body, prowess, Resource.Type.ALLY, true);
+    public Ally(Player player, int mp, int mind, int body, int prowess) {
+        super(player, mp, 0, 0, mind, body, prowess, Resource.Type.ALLY, true);
     }
     
 }
